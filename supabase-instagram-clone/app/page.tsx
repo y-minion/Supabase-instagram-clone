3968c81462ebb35441a5277fc3fa185ea9b3f292
y-minion <dudals2963@tukorea.ac.kr>
2025-10-03 21:33:12 +0900

    인증 로직 구현

--- supabase-instagram-clone/app/page.tsx
+++ supabase-instagram-clone/app/page.tsx
@@ -1,14 +1,22 @@
 import LogoutButton from "components/logout-button";
+import { createServerSupabaseClient } from "utils/supabase/server";
 
 export const metadata = {
   title: "Clonestagram",
   description: "Instagram clone project",
 };
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createServerSupabaseClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <main className="w-full flex flex-col items-center justify-center h-screen gap-2">
-      <h1 className="font-bold text-xl">Welcome {"웅맹잉"}!</h1>
+      <h1 className="font-bold text-xl">
+        Welcome {session?.user?.email?.split("@")?.[0]}!
+      </h1>
       <LogoutButton />
     </main>
   );
---
+++ supabase-instagram-clone/app/signup/confirm/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { createServerSupabaseClient } from "utils/supabase/server";
+
+export async function GET(request: Request) {
+  //리퀘스트를 url 객체로 만들어 준다.
+  const requestUrl = new URL(request.url);
+
+  //쿼리 파람 추출
+  const code = requestUrl.searchParams.get("code");
+
+  //쿼리파람에 code가 존재하면 수파베이스의 세션에 코드를 등록한다.
+  if (code) {
+    const supabase = await createServerSupabaseClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  //위의 모든 로직이 성공적으로 마무리 되면 url객체의 origin(메인 페이지)로 리다이렉트 시킨다.
+  return NextResponse.redirect(requestUrl.origin);
+}
