051ad1f8493fdafcebf1cc5cd489611b31fe7666
y-minion <dudals2963@tukorea.ac.kr>
2025-10-09 12:28:17 +0900

    💄 유저 목록 UI 구현 완료

--- supabase-instagram-clone/components/chat/chatPeopleList.tsx
+++ supabase-instagram-clone/components/chat/chatPeopleList.tsx
@@ -1,28 +1,28 @@
+"use client";
+
+import { selectedIndexState } from "utils/recoil/atoms";
 import Person from "./person";
+import { useRecoilState } from "recoil";
 
 export default function ChatPeopleList() {
+  const [selectedIndex, setSelectedIndex] = useRecoilState(selectedIndexState);
+
   return (
     <div className="h-screen min-w-60 flex flex-col bg-gray-50">
       <Person
+        onClick={() => setSelectedIndex(0)}
         index={0}
-        isActive={true}
+        isActive={selectedIndex === 0}
         name={"이안"}
         onChatScreen={false}
         onlineAt={new Date().toISOString()}
         userId={"ads"}
       />
       <Person
+        onClick={() => setSelectedIndex(1)}
         index={0}
-        isActive={false}
+        isActive={selectedIndex === 1}
         name={"웅이에"}
-        onChatScreen={false}
-        onlineAt={new Date().toISOString()}
-        userId={"ads"}
-      />
-      <Person
-        index={0}
-        isActive={true}
-        name={"오이에"}
         onChatScreen={false}
         onlineAt={new Date().toISOString()}
         userId={"ads"}
