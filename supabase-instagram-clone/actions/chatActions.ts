d12b040f43e5793d6611b10cd4ada3a1cc6342ce
y-minion <dudals2963@tukorea.ac.kr>
2025-10-09 16:58:03 +0900

    ✨ userId 기준으로 유저 인스턴스 불러오는 서버액션 구현

---
+++ supabase-instagram-clone/actions/chatActions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { createServerSupabaseAdminClient } from "utils/supabase/server";
+
+export async function getAllUsers() {
+  //접속을 안 한 유저들의 목록도 모두 가져와야 하므로, admin 권한이 있는 객체를 불러온다
+  const supabase = await createServerSupabaseAdminClient();
+
+  const { data, error } = await supabase.auth.admin.listUsers();
+
+  if (error) {
+    console.error(`에러발생 ${error}`);
+    return [];
+  }
+
+  return data.users;
+}
+
+export async function getUserById(userId: string) {
+  const supabase = await createServerSupabaseAdminClient();
+
+  const { data, error } = await supabase.auth.admin.getUserById(userId);
+  if (error) return null;
+
+  return data.user;
+}
