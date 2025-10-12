37255f84248cb84cb305947bbc6f75e2f626486f
y-minion <dudals2963@tukorea.ac.kr>
2025-10-12 17:41:07 +0900

    ✨ 리얼타임 존재 상태 전파 구현
    동일 채널을 구독하는 컴포넌트에게 각 유저의 존재를 전파하는 로직 구현.
    컴포넌트가 렌더링 될때 Presence 전파 목적 채널을 생성하고, 채널을 구독하면서 현재 유저의 접속 시간을 각 구독 컴포넌트들에게 전파하는 로직 구현완료

--- supabase-instagram-clone/components/chat/chatPeopleList.tsx
+++ supabase-instagram-clone/components/chat/chatPeopleList.tsx
@@ -8,11 +8,15 @@ import Person from "./person";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { useQuery } from "@tanstack/react-query";
 import { getAllUsers } from "actions/chatActions";
+import { useEffect } from "react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function ChatPeopleList({ loggedInUser }) {
   const [selectedUserId, setSelectedUserId] =
     useRecoilState(selectedUserIdState);
   const setSelectedUserIndex = useSetRecoilState(selectedUserIndexState);
+
+  const supabase = createBrowserSupabaseClient();
 
   //getAllUsers
   const getAllUsersQuery = useQuery({
@@ -23,6 +27,38 @@ export default function ChatPeopleList({ loggedInUser }) {
       return allUsers.filter(({ id }) => id !== loggedInUser?.id);
     },
   });
+
+  useEffect(() => {
+    const channel = supabase.channel("online_users", {
+      config: {
+        presence: {
+          //presence를 분류할 수 있는 커스텀 key값 주입
+          key: loggedInUser?.id,
+        },
+      },
+    });
+
+    //같은 채널의 유저의 상태에 대해 구독
+    channel.on("presence", { event: "sync" }, () => {
+      const newState = channel.presenceState();
+      console.log(newState);
+    });
+
+    channel.subscribe(async (status) => {
+      if (status !== "SUBSCRIBED") {
+        return;
+      }
+      //useEffect로 해당 채널에 구독하는 순간에 현재의 시간을 onlineAt 키의 값으로 동일 채널의 모든 구독자들에게 전파
+      const newPresenceStatus = await channel.track({
+        onlineAt: new Date().toISOString(),
+      });
+      console.log(newPresenceStatus);
+    });
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
 
   return (
     <div className="h-screen min-w-60 flex flex-col bg-gray-50">
