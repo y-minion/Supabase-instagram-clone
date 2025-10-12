5bd3e4c77055b6a348d04f929b125ddffeff43a8
y-minion <dudals2963@tukorea.ac.kr>
2025-10-12 14:54:12 +0900

    ✨ 채팅 관련 액션 구현
    유저id에 대한 메세지 데이터를 메세지 테이블에서 받아오는 액션 구현.
    메세지와 전송할 대상을 입력하면 메세지 테이블에 데이터를 저장하는 액션 구현

--- supabase-instagram-clone/actions/chatActions.ts
+++ supabase-instagram-clone/actions/chatActions.ts
@@ -1,6 +1,10 @@
 "use server";
 
-import { createServerSupabaseAdminClient } from "utils/supabase/server";
+import { get } from "http";
+import {
+  createServerSupabaseAdminClient,
+  createServerSupabaseClient,
+} from "utils/supabase/server";
 
 export async function getAllUsers() {
   //접속을 안 한 유저들의 목록도 모두 가져와야 하므로, admin 권한이 있는 객체를 불러온다
@@ -23,4 +27,55 @@ export async function getUserById(userId: string) {
   if (error) return null;
 
   return data.user;
+}
+
+export async function sendMessage({ message, chatUserId }) {
+  const supabase = await createServerSupabaseClient(); // 현재 접속한 계정이 누구인지 알기위해서 반드시 admin이 아닌 일반 서버 클라이언트를 생성해야한다.
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+
+  const { data, error: sendMessageError } = await supabase
+    .from("message")
+    .insert({
+      message,
+      receiver: chatUserId,
+      sender: session.user.id,
+    });
+
+  if (sendMessageError) {
+    throw new Error(sendMessageError.message);
+  }
+
+  return data;
+}
+
+export async function getAllMessages({ chatUserId }) {
+  const supabase = await createServerSupabaseClient();
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+
+  const { data, error: getMessagesError } = await supabase
+    .from("message")
+    .select("*")
+    .or(`receiver.eq.${session.user.id},receiver.eq.${chatUserId}`)
+    .or(`sender.eq.${session.user.id},sender.eq.${chatUserId}`)
+    .order("created_at", { ascending: true });
+
+  if (getMessagesError) return [];
+
+  return data;
 }
